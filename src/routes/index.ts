4773c36d51198e09db677c98447b9ef1529fd5bf
Jose Henrique <jh4832184@gmail.com>
2024-04-21 15:29:27 -0300

    feat: add route taskCreate

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,14 @@ import { FastifyInstance } from "fastify";
 import { userCreateRoute } from "./user/userCreateRoute";
 import { userLoginRoute } from "./user/userLoginRoute";
 
+import { taskCreateRoute } from "./task/taskCreateRoute";
+
+
 export async function routes (server:FastifyInstance) {
     server.register(userCreateRoute, {prefix: "/users"});
     server.register(userLoginRoute, {prefix: "/users"});
+
+    server.register(taskCreateRoute, {prefix: "/tasks"});
 };
 
 //TASK: title, description, colorBackground, isFavorito, idUser ;
